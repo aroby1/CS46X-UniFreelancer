a93a6f3a4aaacbd1958bca36808c340638b7a3b3
Aiden McCoy <mccoaide@oregonstate.edu>
2026-02-10 17:35:19 -0800

    Working on getting individual courses working.

--- server/client/src/pages/Academy/Courses/CreateCourse.jsx
+++ server/client/src/pages/Academy/Courses/CreateCourse.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './CreateCourse.css';
-import ImageUpload from '../../components/ImageUpload';
+import ImageUpload from '../../../components/ImageUpload';
 
 function CreateCourse() {
   const navigate = useNavigate();
